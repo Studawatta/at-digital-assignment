5afd1cc2b04438dfa59dcc3de8d3dcc055599d4c
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-04-08 10:57:59 +0530

    create navbar, button, footer components and landing page

---
+++ src/components/TopNavigation.jsx
@@ -0,0 +1,5 @@
+const TopNavigation = () => {
+  return <div>TopNavigation</div>;
+};
+
+export default TopNavigation;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer';
+import TopNavigation from '../components/TopNavigation';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <TopNavigation />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
